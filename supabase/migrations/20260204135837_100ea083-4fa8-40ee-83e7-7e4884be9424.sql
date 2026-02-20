a0cfd20caba01c8fa21017a789464062f846f67b
dragonairobot12-wq <mohammadyasin8590@gmail.com>
2026-02-21 01:04:43 +0300

    Delete supabase directory

--- supabase/migrations/20260204135837_100ea083-4fa8-40ee-83e7-7e4884be9424.sql
+++
@@ -1,8 +0,0 @@
--- Enable realtime for wallets table
-ALTER PUBLICATION supabase_realtime ADD TABLE public.wallets;
-
--- Enable realtime for deposits table
-ALTER PUBLICATION supabase_realtime ADD TABLE public.deposits;
-
--- Enable realtime for withdrawals table
-ALTER PUBLICATION supabase_realtime ADD TABLE public.withdrawals;
